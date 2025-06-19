8aa693dd2a43e11eaab7367adb4643d42bb486ac
Abhijith01-dot <abhijithm477@gmail.com>
2025-06-19 09:46:26 +0400

    chat

--- lib/Chats.dart
+++ lib/Chats.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class Chat extends StatelessWidget {
- 
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: Color.fromARGB(255, 167, 164, 164),);
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 245, 242, 242),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 220, 220, 243), 
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/MESSI.jpg"),
+                ),
+                title: Text("Lionel Messi"),
+                subtitle: Text("Hello"),
+                trailing: Text("9:45 PM"),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 5),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 221, 221, 244), 
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/ronaldo.webp"),
+                ),
+                title: Text("Cristiano Ronaldo"),
+                subtitle: Text("Hey"),
+                trailing: Text("9:58 PM"),
+              ),
+              margin: EdgeInsets.symmetric(vertical: 5),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
